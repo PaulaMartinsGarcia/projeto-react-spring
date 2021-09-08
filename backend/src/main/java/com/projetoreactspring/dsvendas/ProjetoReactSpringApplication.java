c1900d13835530b267e83ba34124b2b4b1be1e80
PaulaMartinsGarcia <paulastsmartins@globo.com>
2021-09-08 08:46:26 -0300

    Project created

---
+++ backend/src/main/java/com/projetoreactspring/dsvendas/ProjetoReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projetoreactspring.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoReactSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetoreactspring/dsvendas/ProjetoReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoreactspring.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
